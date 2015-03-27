9c64296fd1b8dc64b202d1d763cd0f8a64fb10f2
Yang Li <idealhack@gmail.com>
2015-03-28 00:32:52 +0800

    update w/ tusdk 1.7.0

--- TuSDK/src/org/lasque/tusdk/TuApplication.java
+++ TuSDK/src/org/lasque/tusdk/TuApplication.java
@@ -27,12 +27,12 @@ public class TuApplication extends TuSdkApplication
 		// 设置输出状态
 		this.setEnableLog(true);
 		// 初始化SDK (请前往 http://tusdk.com 获取您的APP 开发秘钥)
-		this.initPreLoader(this.getApplicationContext(), "57ba8da77fb46580-04");
+		this.initPreLoader(this.getApplicationContext(), "12aa4847a3a9ce68-04-ewdjn1");
 
 		// 如果不想继承TuApplication，直接在自定义Application.onCreate()方法中调用以下方法
 		// 初始化全局变量
 		// TuSdk.enableDebugLog(true);
 		// 开发ID (请前往 http://tusdk.com 获取您的APP 开发秘钥)
-		// TuSdk.init(this.getApplicationContext(), "57ba8da77fb46580-04");
+		// TuSdk.init(this.getApplicationContext(), "12aa4847a3a9ce68-04-ewdjn1");
 	}
 }
